df3b35c18c29a3b67391d53d55da2d12e0cbab95
httpsdinha <amandabalcacar@gmail.com>
2025-04-14 09:53:32 -0400

    style: adicionando header

--- app/src/main/java/com/example/receitasdeliciosas/MainActivity.java
+++ app/src/main/java/com/example/receitasdeliciosas/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.receitasdeliciosas;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Configura a Toolbar como ActionBar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         // Configura a navegação
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        //NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
     @Override
